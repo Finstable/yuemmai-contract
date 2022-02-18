08bd113e762b17d8501f9b371518c7aa01f48afc
tanakorn0412@gmail.com <tanakorn0412@gmail.com>
2022-02-18 11:59:53 +0700

    publish repo

--- scripts/deploy/main.ts
+++ scripts/deploy/main.ts
@@ -1,14 +1,10 @@
 import { deployYESToken } from "./deploy-yes-token";
 import { deployController } from "./deploy-controller";
 import { deployInterest } from "./deploy-interest";
-import { deployKUBLending } from "./deploy-kublending";
 import { deployMarketImpl } from "./deploy-marketImpl";
 import { deployOracle } from "./deploy-oracle";
 import { deployVault } from "./deploy-vault";
 import { deployKAP20Lending } from "./deploy-kap20lending";
-import { deployBorrowLimit } from "./deploy-borrow-limit";
-import { deployYESAdmin } from "./deploy-yes-admin";
-import { deployTestEnv } from "./deploy-test-env";
 
 const deployLendingContracts = async () => {
   await deployKAP20Lending("KUSDT");
